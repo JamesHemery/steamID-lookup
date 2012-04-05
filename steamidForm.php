89da5f269a1a2dc119cf59f82329bfd7bcd00f68
Austin Reuland <amreuland@gmail.com>
2012-04-05 02:19:00 -0500

    Update steamidForm.php

--- steamidForm.php
+++ steamidForm.php
@@ -6,9 +6,10 @@
 </form><br>
 <div id="ResponseDiv" class="ResponseDiv" height="1100px">
 </div>
-<p>Version 1.9 - <a href="https://github.com/DJWolf/steamID-lookup" target="_blank">Public Release</a>
+<p>Version 1.9 - <a href="https://github.com/DJWolf/steamID-lookup" target="_blank">Public Release on GitHub</a>
 <br>Steam Player/ID Lookup. By <a href="http://steamcommunity.com/profiles/76561198027173954" title="DJ Wolf">DJ Wolf</a> - 
 <!--DO NOT REMOVE THE LINE UNDER THIS!!! - STEAM REQUIRES IT IF YOU ARE GOING TO USE THE STEAM API OR COMMUNITY DATA-->
 <a href="http://steampowered.com" target="_blank">Powered By Steam</a></p> 
 </CENTER>
 <?php if(isset($_GET['steam'])){echo '<script type="text/javascript">MakeSteamidRequest("'.$_GET['steam'].'");</script>';}?>
+
